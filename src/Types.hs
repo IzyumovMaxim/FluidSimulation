7c6815d5fe6c04b9f17ed15ea5d37110c41849a8
RII <104525820+RII6@users.noreply.github.com>
2025-07-11 21:55:59 +0300

    Update Types.hs

--- src/Types.hs
+++ src/Types.hs
@@ -20,3 +20,11 @@ data World = World
   , mousePos  :: Vector2  -- Add mouse position
   , mouseDown :: Bool     -- Add mouse state
   }
+
+-- Generate initial particles based on current world parameters
+generateInitialParticles :: World -> [Particle]
+generateInitialParticles world = 
+  [ Particle (x, y) (0,0) 0 0
+   | x <- [-50, -45..50]   -- Step 2
+   , y <- [80, 85..140]    -- Step 2
+   , (x*x + (y-110)*(y-110)) <= 50*50 ]
